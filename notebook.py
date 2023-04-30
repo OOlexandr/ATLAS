941f09d80a1f6040d444c41f633f7c1d51f463b7
GievskiyIgor <121798393+GievskiyIgor@users.noreply.github.com>
2023-05-01 00:08:28 +0300

    addnotetag_chnote
    Добавил функции add_note_tag и change_note, обработки вызов команды add_note_tag и note_change

--- notebook.py
+++ notebook.py
@@ -7,14 +7,41 @@ class InvalidTagError(Exception):
 class InvalidNoteError(Exception):
     pass
 
+class Title(Field): #Gievskiy 
+    pass
+
 class Tag(Field):
-    def is_valid(self, value :str):
-        #conditions are: a tag must be a str and it's length must be less than 10 symbols
-        #I think, it makes sense that tags must be short
-        if type(value) == str and len(value) <= 10:
-            return True
+    # Gievskiy 30042023
+    def __init__(self, value=None):
+        self.value = value
+    
+    @property
+    def value(self):
+        return self._value
+        
+    @value.setter
+    def value(self, value):
+        if value is None:
+            self._value = value
         else:
+            self._value = self.is_valid(value)
+    # **** 30042023
+    
+    # def is_valid(self, value: str):
+    def is_valid(self, tag: str):
+        # conditions are: a tag must be a str and it's length must be less than 10 symbols
+        # I think, it makes sense that tags must be short
+        
+        if not type(tag) == str:
+            raise ValueError
+        elif len(tag) > 10:
             raise InvalidTagError
+        return tag
+        
+        # if type(value) == str and len(value) <= 10:
+        #     return True
+        # else:
+        #     raise InvalidTagError
 
 class NoteText(Field):
     def is_valid(self, value :str):
@@ -25,12 +52,33 @@ class NoteText(Field):
             raise InvalidNoteError
 
 class Note:
-    def __init__(self, text :NoteText, tags :list[Tag] = None) -> None:
+    # Gevskiy 30042023 
+    def __init__(self, text: NoteText, tag:Tag = None) -> None:
         self.text = text
-        self.tags = []
-        if tags:
-            self.tags += tags
+        self.tags = [tag] if tag else []
     
+    # def __init__(self, text: NoteText, tags: list[Tag] = None) -> None:
+    #     self.text = text
+    #     self.tags = []
+    #     if tags:
+    #         self.tags += tags
+    #  **** 30042023
+    
+    def add_note_tag(self, tag:Tag):
+        self.tags.append(tag)
+    
+    def change_note(self, old_text:NoteText, new_text:NoteText, old_tag:Tag, new_tag:Tag):
+        for i, p in enumerate(self.title):
+            if p.value == old_text.value:
+                self.title[i] = new_text
+                return f'Phone {old_text} change to {new_text}'
+            
+            if p.value == old_tag.value:
+                self.title[i] = new_tag
+                return f'Phone {old_tag} change to {new_tag}'
+        return f'{old_text} - {new_text}, {old_tag} - {new_tag}'  
+    #  **** 30042023
+        
     @property
     def text(self):
         return self.__text
@@ -41,4 +89,62 @@ class Note:
             self.__text = text
 
 class Notebook(UserList):
-    pass
+    def __init__(self):
+        super().__init__()
+
+    def add_note(self, text: NoteText, *tags: Tag) -> Note:
+        note = Note(text, tags)
+        self.data.append(note)
+        return note
+
+#  Gievskiy 300420223
+#  functions
+# Decorator input errors
+notebook = Notebook ()
+
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            raise KeyError
+        except ValueError:
+            raise ValueError
+        except IndexError:
+            raise IndexError
+        except TypeError:
+            raise TypeError
+    return wrapper
+
+# Add an entry with a tag
+@input_error
+def add_note_tag(*args, **kwargs):
+    title = Title(args[0])
+    text = NoteText(args[1])
+    tag = Tag(args[2])
+        
+    rec = notebook.get(title.value)
+    if not rec:
+        rec: Note = Note(title,  text, tag)
+        notebook.add_note(rec)
+    
+    rec.add_note_tag(tag)
+    return f"{title} : {text}, {tag} has been added to the NoteBook"
+
+# text change
+@input_error
+def note_change(*args, **kwargs):
+    title = Title(args[0])
+    old_text = NoteText(args[1])
+    new_text = NoteText(args[2])
+    old_tag = NoteText(args[3])
+    new_tag = NoteText(args[4])
+    
+    rec = notebook.get(title.value)
+    
+    if rec:
+        return rec.change_note(old_text, new_text, old_tag, new_tag)
+    
+    return f'NoteBook has no contact {title}'
+# *** 30042023
+
