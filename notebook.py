84601fd770733d8a4d1b3c10e77c6e757afeb850
Yevheniy Petrenko <yevheniipetrenko92@gmail.com>
2023-04-30 12:10:14 +0200

    Додав Функцію add_note

--- notebook.py
+++ notebook.py
@@ -1,44 +1,56 @@
 from collections import UserList
 from field import Field
 
+
 class InvalidTagError(Exception):
     pass
+
 
 class InvalidNoteError(Exception):
     pass
 
+
 class Tag(Field):
-    def is_valid(self, value :str):
-        #conditions are: a tag must be a str and it's length must be less than 10 symbols
-        #I think, it makes sense that tags must be short
+    def is_valid(self, value: str):
+        # conditions are: a tag must be a str and it's length must be less than 10 symbols
+        # I think, it makes sense that tags must be short
         if type(value) == str and len(value) <= 10:
             return True
         else:
             raise InvalidTagError
 
+
 class NoteText(Field):
-    def is_valid(self, value :str):
-        #The only condition is that the tag must be a string. For now at least
+    def is_valid(self, value: str):
+        # The only condition is that the tag must be a string. For now at least
         if type(value) == str:
             return True
         else:
             raise InvalidNoteError
 
+
 class Note:
-    def __init__(self, text :NoteText, tags :list[Tag] = None) -> None:
+    def __init__(self, text: NoteText, tags: list[Tag] = None) -> None:
         self.text = text
         self.tags = []
         if tags:
             self.tags += tags
-    
+
     @property
     def text(self):
         return self.__text
-    
+
     @text.setter
     def text(self, text):
         if type(text) == NoteText:
             self.__text = text
 
+
 class Notebook(UserList):
-    pass
+    def __init__(self):
+        super().__init__()
+
+    def add_note(self, text: NoteText, *tags: Tag) -> Note:
+        note = Note(text, tags)
+        self.data.append(note)
+        return note
