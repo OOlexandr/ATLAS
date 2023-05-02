9af03378e9ca0a8de3fe68997fad242a6551c9dd
GievskiyIgor <121798393+GievskiyIgor@users.noreply.github.com>
2023-05-03 02:33:01 +0300

    add_note_tag_1
    Переработал код, проверил, работает

--- tester.py
+++ tester.py
@@ -34,7 +34,10 @@ commands = [("hello", "gretting"),
             ("show all", "shows with division on pages"),
             ("find not", "not found"),
             ("find a", "found carl, sam"),
-            ("find 60-14", "finds johny")]
+            ("find 60-14", "finds johny"),
+            ("add note Igor Hello", "add note"),  #Gievskiy 05022023
+            ("add tag Igor tag1", "add tag")  #Gievskiy 05022023
+            ] 
 #Бачу можливість зробити вивід зручнішим для читання. 
 #Замість того, щоб всі команди були в одному списку можна розбити його на підсписки,
 #кожен з яких буде відповідати за тестування певного модуля.
