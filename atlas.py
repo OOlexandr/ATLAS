e5c3221e7399a98a21d957c868ad5d169ee72d16
Oleksandr <115171014+OleksandrPiatnytsia@users.noreply.github.com>
2023-05-02 18:02:33 +0300

    Добавив пдказки в каскадне меню

--- atlas.py
+++ atlas.py
@@ -217,20 +217,20 @@ def delete_note(args):
 
 
 handlers = {"hello": {"func": handler_greetings, "help_message": "Just greeting!"},
-            "good bye": {"func": handler_exit, "help_message": "exit from bot"},
+            "goodbye": {"func": handler_exit, "help_message": "exit from bot"},
             "close": {"func": handler_exit, "help_message": "exit from bot"},
             "exit": {"func": handler_exit, "help_message": "exit from bot"},
-            "add record": {"func": handler_add, "help_message": "add record ContactName ContactPhone Contactbirthday"},
-            "add birthday": {"func": handler_add_birthday, "help_message": "add birthday ContactName Contactbirthday",
+            "addrecord": {"func": handler_add, "help_message": "add record ContactName ContactPhone Contactbirthday"},
+            "addbirthday": {"func": handler_add_birthday, "help_message": "add birthday ContactName Contactbirthday",
                              "from_data": contacts},
-            "add phone": {"func": handler_add_phone, "help_message": "add phone ContactName ContactPhone",
+            "addphone": {"func": handler_add_phone, "help_message": "add phone ContactName ContactPhone",
                           "from_data": contacts},
             "change": {"func": handler_change, "help_message": "change ContactName OldPhone NewPhone",
                        "from_data": contacts},
             "phone": {"func": handler_phone, "help_message": "phone ContactName", "from_data": contacts},
-            "days to birthday": {"func": handler_days_to_birthday, "help_message": "days to birthday ContactName"},
-            "show all": {"func": handler_show_all, "help_message": "showed all contacts"},
-            "findnote": {"func": find_note, "help_message": "find NoteText", "from_data": notes},
+            "daystobirthday": {"func": handler_days_to_birthday, "help_message": "days to birthday ContactName"},
+            "showall": {"func": handler_show_all, "help_message": "showed all contacts"},
+            "findnote": {"func": find_note, "help_message": "find NoteText"}, #"from_data": notes},
             "find": {"func": find, "help_message": "find ContactName", "from_data": contacts},
             "sort": {"func": sort, "help_message": "sort FolderPath"},
             "delnote": {"func": delete_note, "help_message": "delnote NoteName", "from_data": notes}}
@@ -261,13 +261,17 @@ comands_list_meta_dict = {}
 
 
 def create_completer_data():
+    global comands_nested_dict
+    global comands_list_meta_dict
+    global comands_list
+
     if use_nested_completer:
-        global comands_nested_dict
+
         for k, v in handlers.items():
             comands_nested_dict.update({k: None})
+            comands_list_meta_dict.update({k: v["help_message"]})
     else:
-        global comands_list
-        global comands_list_meta_dict
+
         for k, v in handlers.items():
             comands_list.append(k)
             comands_list_meta_dict.update({k: v["help_message"]})
@@ -281,10 +285,15 @@ def update_nested_dict():
 
     for command_name, params_dict in handlers.items():
         from_data = params_dict.get("from_data")
+
+        _meta_dict = {}
+
         if from_data:
             # print(f"command_name:{command_name} : {from_data.get_data_list()}")
+            for note_name in from_data.get_data_list():
+                _meta_dict.update({note_name: comands_list_meta_dict.get(command_name)})
 
-            comands_nested_dict[command_name] = WordCompleter(from_data.get_data_list(), match_middle=True)
+            comands_nested_dict[command_name] = WordCompleter(from_data.get_data_list(), match_middle=True, sentence=True, meta_dict=_meta_dict)
 
 
 def main():
