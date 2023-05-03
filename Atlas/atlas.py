c1945f099d996dbbcd75eab9a5e2fd2b893c96be
Alex <94902688+OOlexandr@users.noreply.github.com>
2023-05-04 00:22:27 +0300

    package3

--- Atlas/atlas.py
+++ Atlas/atlas.py
@@ -1,10 +1,13 @@
 import address_book
-import Atlas.notebook as notebook
-from Atlas.move_main import main_sort, InvalidPath
-
+from notebook import *
+from move_main import main_sort, InvalidPath
+import csv
 from prompt_toolkit import PromptSession
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.completion import NestedCompleter
+
+from config import use_promt_toolkit, pre_command_text, use_nested_completer
 
 
 class NameNotGivenError(Exception):
@@ -31,8 +34,12 @@ class NoteNameNotGivenError(Exception):
     pass
 
 
+class ContctNameNotGivenError(Exception):
+    pass
+
+
 contacts = address_book.AddressBook()
-notes = notebook.Notebook()
+notes = Notebook()
 
 
 def error_handler(func):
@@ -67,6 +74,8 @@ def error_handler(func):
             return "Please enter text to find"
         except NoteNameNotGivenError:
             return "Please enter note name to delete"
+        except ContctNameNotGivenError:
+            return "Please enter contact name to find"
 
     return inner
 
@@ -76,6 +85,18 @@ def error_handler(func):
 @error_handler
 def handler_greetings(args):
     return "How can I help you?"
+
+
+@error_handler
+def handler_addnote(args):
+    if len(args) < 2:
+        raise TextNotGivenError
+    title = Name(args[0])
+    text = NoteText(' '.join(args[1:]))
+    note = Note(name=title, text=text)
+    notes.append(note)
+    notes.save_notes_to_file()
+    return "Note added successfully"
 
 
 @error_handler
@@ -167,6 +188,8 @@ def handler_show_all(args):
 
 @error_handler
 def find(args):
+    if len(args) == 0:
+        raise ContctNameNotGivenError
     records = contacts.find_records(args[0])
     if records:
         message = "Found contacts are:"
@@ -179,14 +202,43 @@ def find(args):
     else:
         return "No contacts were found"
 
+@error_handler
+def export(args):
+    if contacts:
+
+        with open("contacts.csv", "w", newline="") as is_file:
+            fieldnames = ["Name", "Phone", "Email"]
+            writer = csv.DictWriter(is_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for name, info in contacts.items():
+                writer.writerow({"Name": name, "Phone": info["phone"], "Email": info["email"]})
+
+        return "Contacts successfully exported to file contacts.csv."
+    else:
+        return "Contact list is empty."
+
 
 @error_handler
 def sort(args):
     if len(args) == 0:
         raise PathNotGivenError
     main_sort(args[0])
-    return "Files sorted succesfully"
+    return "Files sorted successfully"
 
+
+
+# my code
+@error_handler
+def handler_add_note(args):
+    if len(args) < 2:
+        raise TextNotGivenError
+    title = Name(args[0])
+    text = NoteText(' '.join(args[1:]))
+    note = Note(name=title, text=text)
+    notes.append(note)
+    notes.save_notes_to_file()
+    return "Note added successfully"
 
 @error_handler
 def find_note(args):
@@ -197,7 +249,7 @@ def find_note(args):
     if found_notes:
         message = "found notes are:\n"
         for n in found_notes:
-            message += "\n" + n.text.value
+            message += "\n" + str(n)
         return message
     return "No notes found"
 
@@ -211,6 +263,29 @@ def delete_note(args):
         return f"Note {note_name} successfully deleted"
     else:
         return f"Can't delete note: {note_name}!"
+
+
+@error_handler
+def sortnote(args):
+    if len(args)==0:
+        return "Please give sorting criteria"
+    rev = False
+    if len(args)>=2:
+        if args[1] == "dec":
+            rev = True
+    if args[0] == 'name':
+        notes.sort(reverse = rev, key = lambda n: n.name.value)
+    elif args[0] == 'text':
+        notes.sort(reverse = rev, key = lambda n: n.text.value)
+    else:
+        return "Key is invalid"
+    return "Successfully sorted"
+
+
+@error_handler
+def reference(args):
+    with open('readme.txt', encoding="utf-8") as file:
+        return file.read()
 
 
 handlers = {"hello": {"func": handler_greetings, "help_message": "Just greeting!"},
@@ -227,7 +302,10 @@ handlers = {"hello": {"func": handler_greetings, "help_message": "Just greeting!
             "find note": {"func": find_note, "help_message": "find NoteText"},
             "find": {"func": find, "help_message": "find ContactName"},
             "sort": {"func": sort, "help_message": "sort FolderPath"},
-            "delnote": {"func": delete_note, "help_message": "delnote NoteName"}}
+            "delnote": {"func": delete_note, "help_message": "delnote NoteName"},
+            "help": {"func": reference, "help_message": "help NoteName"},
+            "export": {"func": export, "help_message": "export NoteName"},
+            "add note": {"func": handler_addnote, "help_message": "add note name text"}}
 
 
 # key - command, value - handler.
@@ -249,23 +327,71 @@ def parce(command):
     return None
 
 
+comands_nested_dict = {}
 comands_list = []
 comands_list_meta_dict = {}
 
-for k, v in handlers.items():
-    comands_list.append(k)
-    comands_list_meta_dict.update({k: v["help_message"]})
+
+def create_completer_data():
+    global comands_nested_dict
+    global comands_list_meta_dict
+    global comands_list
+
+    if use_nested_completer:
+
+        for k, v in handlers.items():
+            comands_nested_dict.update({k: None})
+            comands_list_meta_dict.update({k: v["help_message"]})
+    else:
+
+        for k, v in handlers.items():
+            comands_list.append(k)
+            comands_list_meta_dict.update({k: v["help_message"]})
+
+
+def update_nested_dict():
+    global comands_nested_dict
+
+    for command_name, params_dict in handlers.items():
+        from_data = params_dict.get("from_data")
+
+        if from_data:
+
+            meta_dict = {}
+
+            for note_name in from_data.get_data_list():
+                meta_dict.update({note_name: comands_list_meta_dict.get(command_name)})
+
+            comands_nested_dict[command_name] = WordCompleter(from_data.get_data_list(), match_middle=True,
+                                                              sentence=True, meta_dict=meta_dict)
+        nested_dict = params_dict.get("nested_dict")
+        if nested_dict:
+            comands_nested_dict[command_name] = nested_dict
 
 
 def main():
-    session = PromptSession()
     contacts.read_contacts()
     notes.load_notes_from_file()
+
+    if use_promt_toolkit:
+        create_completer_data()
+        session = PromptSession()
+
     while True:
 
-        input_text = session.prompt('Input command >>> ', auto_suggest=AutoSuggestFromHistory(),
-                                    completer=WordCompleter(comands_list, meta_dict=comands_list_meta_dict,
-                                                            sentence=True))
+        if use_promt_toolkit:
+
+            update_nested_dict()
+
+            if use_nested_completer:
+                input_text = session.prompt(pre_command_text, auto_suggest=AutoSuggestFromHistory(),
+                                            completer=NestedCompleter.from_nested_dict(comands_nested_dict))
+            else:
+                input_text = session.prompt(pre_command_text, auto_suggest=AutoSuggestFromHistory(),
+                                            completer=WordCompleter(comands_list, meta_dict=comands_list_meta_dict,
+                                                                    sentence=True))
+        else:
+            input_text = input(pre_command_text)
 
         command = parce(input_text)
         if command:
