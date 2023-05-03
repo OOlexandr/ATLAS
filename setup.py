79a5ba64192446b5ddd90bff2bcca98c0c2fdffa
Alex <94902688+OOlexandr@users.noreply.github.com>
2023-05-04 00:27:08 +0300

    pack4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_namespace_packages(),
     entry_points={
    'console_scripts': [
-       'atlas = ATLAS.atlas:main',
+       'atlas = Atlas\ATLAS.atlas:main',
        ]
    },
    install_requires=['prompt_toolkit'],
